a975b58fad5b410141c2c03eb9034bd373360f92
crunck78 <crunck78@googlemail.com>
2022-01-18 20:45:13 +0100

    4 files modified

--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -12,29 +12,21 @@ import { NavigationService } from '../navigation.service';
   templateUrl: './head.component.html',
   styleUrls: ['./head.component.css'],
 })
-export class HeadComponent implements OnInit, AfterViewInit {
-  @ViewChild('text') text: ElementRef;
-  newDom = '';
-  animationDelay = 6;
-
-  introText = 'Hi, My Name is Mihai-Andrei Neacsu. I am a Front-End Developer.';
+export class HeadComponent implements OnInit {
+  /**
+   * We need the text as a Character Array for *ngFor to work. ['H', 'i', ...]
+   */
+  introText = [
+    ...'Hi, My Name is Mihai-Andrei Neacsu. I am a Front-End Developer.',
+  ];
 
   constructor(public navigation: NavigationService) {}
-  ngAfterViewInit(): void {
-    for (let i = 0; i < this.introText.length; i++) {
-      this.newDom +=
-        '<span class="char">' +
-        (this.introText[i] == ' ' ? '&nbsp;' : this.introText[i]) +
-        '</span>';
-    }
 
-    this.text.nativeElement.innerHTML = this.newDom;
-    var length = this.text.nativeElement.children.length;
-
-    for (let i = 0; i < length; i++) {
-      this.text.nativeElement.children[i].style['animation-delay'] =
-        this.animationDelay * i + 'ms';
-    }
+  /**
+   * @param {number} - index position of letter
+   */
+  getAnimationDelay(index: number) {
+    return `${10 * index}ms`;
   }
 
   ngOnInit() {}
