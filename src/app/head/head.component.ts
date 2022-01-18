bb69e8ff4f580e7d2fff08c97ccf94615c6e9905
crunck78 <crunck78@googlemail.com>
2022-01-18 15:04:07 +0100

    4 files modified

--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { NavigationService } from '../navigation.service';
 
 @Component({
@@ -6,13 +12,31 @@ import { NavigationService } from '../navigation.service';
   templateUrl: './head.component.html',
   styleUrls: ['./head.component.css'],
 })
-export class HeadComponent implements OnInit {
+export class HeadComponent implements OnInit, AfterViewInit {
+  @ViewChild('text') text: ElementRef;
+  newDom = '';
+  animationDelay = 6;
+
   introText =
     'Hi, My Name is Mihai-Andrei Neacsu.<br> I am a Front-End Developer.';
 
   constructor(public navigation: NavigationService) {}
+  ngAfterViewInit(): void {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    for (let i = 0; i < this.introText.length; i++) {
+      this.newDom +=
+        '<span class="char">' +
+        (this.introText[i] == ' ' ? '&nbsp;' : this.introText[i]) +
+        '</span>';
+    }
 
- 
+    this.text.nativeElement.innerHTML = this.newDom;
+    var length = this.text.nativeElement.children.length;
+
+    for (let i = 0; i < length; i++) {
+      this.text.nativeElement.children[i].style['animation-delay'] =
+        this.animationDelay * i + 'ms';
+    }
+  }
 }
